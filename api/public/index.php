59fcf869061a230812465a327f99561aa66c306f
kento-ix <kentoix1105@gmail.com>
2025-08-22 23:30:17 -0700

    update api

--- api/public/index.php
+++ api/public/index.php
@@ -6,17 +6,18 @@ $path = trim(str_replace('/index.php', '', $uri), '/');
 $action = $_GET['action'] ?? null;
 $method = $_SERVER['REQUEST_METHOD'];
 
+var_dump($uri, $path); exit;
 switch (true) {
-    case str_starts_with($path, 'auth/'):
+    case str_starts_with($path, 'auth'):
         require_once __DIR__ . '/../auth/auth.php';
         break;
-    case str_starts_with($path, 'events/'):
+    case str_starts_with($path, 'events'):
         require_once __DIR__ . '/../routes/events.php';
         break;
-    case str_starts_with($path, 'eventParticipants/'):
+    case str_starts_with($path, 'eventParticipants'):
         require_once __DIR__ . '/../routes/eventParticipants.php';
         break;
-    case str_starts_with($path, 'experiences/'):
+    case str_starts_with($path, 'experiences'):
         require_once __DIR__ . '/../routes/experiences.php';
         break;
     default:
